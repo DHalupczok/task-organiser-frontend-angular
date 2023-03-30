22eedb39908742eeb49c4a7adbe022c20f55da8a
Denis Halupczok <denis.halupczok.external@atos.net>
2023-03-30 20:02:39 +0200

    Bootstrapping an application using a standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [MatSlideToggleModule],
 })
 export class AppComponent {
   title = 'task-organiser-frontend-angular';
